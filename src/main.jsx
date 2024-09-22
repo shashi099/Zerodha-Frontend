8c1c99b90eb8c95989dd29f95e1a191079798e23
Shashi Singh <shashisingh25494@gmail.com>
2024-09-23 01:35:44 +0530

    Start Journey Zerodha

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div>
+      <h1>Start Journey</h1>
+    </div>
+   </StrictMode>,
+)
